e27e618489ca5b2f8c6ede8f5893d7b8cd1e8c87
AnnaKucherenko <annakucherenko31@gmail.com>
2022-07-02 19:13:09 +0300

    зміна посилання

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -42,7 +42,7 @@ export default function ContactsPage(){
         <h2 className={style.title}>Контакти</h2>
         <Filter />
         {isLoader&&<Loader/>}
-        <ContactList  />
+        <ContactList />
     </div>
     )
 }
