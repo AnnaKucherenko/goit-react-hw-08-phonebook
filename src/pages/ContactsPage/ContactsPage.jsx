7262711f0f00247398b89e8e62bc3db3459d3f25
AnnaKucherenko <annakucherenko31@gmail.com>
2022-08-19 16:24:08 +0300

    логотип на сторінці контактів, баг з фавіконом

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,11 +1,12 @@
 import React  from 'react';
 import { useState} from 'react';
+import {  useSelector } from 'react-redux';
 import ContactList from "../../components/ContactsList/ContactList";
 import {Filter} from '../../components/FilterContacts/Filter';
 import { Loader } from "components/Loader/Loader";
 import ModalAddContact from 'components/Modal/ModalAddContact';
-import {  useSelector } from 'react-redux';
-import style from '../ContactsPage/ContactsPage.module.css'
+import {ContactsSvgSelector} from './ContactsSvgSelector';
+import style from '../ContactsPage/ContactsPage.module.css';
 
 
 export default function ContactsPage(){
@@ -20,8 +21,13 @@ export default function ContactsPage(){
     <div className={style.container}>
         {showModal&&<ModalAddContact  onClose={toggleModal}/>}
         <button className={style.button} onClick={()=>toggleModal()}>Додати контакт</button>
-                
-        <h2 className={style.title}>Контакти</h2>
+        <div className={style.containerTitle}>
+            <div className={style.containerLogo}>
+                <ContactsSvgSelector id='address-book' />        
+            </div>
+            <h2 className={style.title}>Контакти</h2>
+        </div>
+        
         <Filter />
         {isLoader&&<Loader/>}
         <ContactList />
